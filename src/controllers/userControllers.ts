aef287b425ae2526fc0d1187aea168629b3c4ee3
Jn_Sajol <julkar10121@gmail.com>
2025-02-03 22:29:00 +0600

    user signup done

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { prisma } from "../Db/db.config";
-import { hashSync } from "bcrypt";
+import { compare, hashSync } from "bcrypt";
 import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (req: Request, res: Response) => {
     const {
@@ -46,4 +47,49 @@ export const createUser = async (req: Request, res: Response) => {
         });
       }
     };
+
+
+    //LOginUser
+    export const loginUser = async (req: Request, res: Response) => {
+        try {
+          const { email, password } = req.body;
+          if (!email && !password) {
+            throw new Error("email and name is required");
+          }
+          const user = await prisma.user.findFirst({
+            where: {
+              email: email,
+            },
+          });
+      
+          if (!user) {
+            throw new Error("User not Found");
+          }
+      
+          const valid = await compare(password, user.password);
+          if (!valid) {
+            throw new Error("Invalid Credential");
+          }
+      
+          const token = jwt.sign({ id: user.id }, "secretkey", {
+            expiresIn: "24h",
+          });
+          res.status(StatusCodes.OK).json({
+            success: true,
+            message: "User Login Successfully",
+            user: {
+              id: user.id,
+              name: user.username,
+              email: user.email,
+            },
+            token: token,
+          });
+        } catch (error: any) {
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: error && error.message ? error.message : "server error",
+          });
+        }
+      };
+      
     
